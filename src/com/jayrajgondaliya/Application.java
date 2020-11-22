8b0ec69e8fd3569ac79265874861a19ba04afc8a
Jayraj Gondaliya <jayrajgondaliya@gmail.com>
2020-11-22 13:00:02 -0800

    Initial Commit

---
+++ src/com/jayrajgondaliya/Application.java
@@ -0,0 +1,7 @@
+package com.jayrajgondaliya;
+
+public class Application {
+	public static void main(String args[]) {
+		System.out.println("Application startup for algorithms and Data Structures..");
+	}
+}

056f92e1c64cdb7a91b5c5d912cb9e4858eaa315
Jayraj Gondaliya <jayrajgondaliya@gmail.com>
2020-11-23 17:06:01 -0800

    Editing SinglyLinkedList

--- src/com/jayrajgondaliya/datastructures/List/SinglyLinkedList.java
+++ src/com/jayrajgondaliya/datastructures/List/SinglyLinkedList.java
@@ -2,8 +2,24 @@ package com.jayrajgondaliya.datastructures.List;
 
 import java.util.Iterator;
 
+/**
+ * @author Jayraj Gondaliya
+ * 
+ * @version 1.0
+ *
+ * @param <T>
+ *
+ *            This class file contains implementation of Singly Linked List, it
+ *            is a list that have support for forward traversal only, - Dynamic
+ *            in Size - Can have any number of elements
+ * 
+ * 
+ */
 public class SinglyLinkedList<T> implements Iterable<T> {
 
+	/**
+	 * An Integer to keep track of currently available number of elements.
+	 */
 	private int size = 0;
 	private Node<T> head = null;
 
@@ -23,6 +39,9 @@ public class SinglyLinkedList<T> implements Iterable<T> {
 		}
 	}
 
+	/**
+	 * Clear an Singly Linked List by deleting or setting all nodes to null.
+	 */
 	public void clear() {
 		Node<T> traverse = head;
 		while (traverse != null) {
@@ -35,14 +54,30 @@ public class SinglyLinkedList<T> implements Iterable<T> {
 		size = 0;
 	}
 
+	/**
+	 * Returns an integer, describing available no of elements.
+	 * 
+	 * @return size
+	 */
 	public int size() {
 		return size;
 	}
 
+	/**
+	 * Returns boolean value representing whether Singly Linked List is Empty.
+	 * 
+	 * @return boolean
+	 */
 	public boolean isEmpty() {
 		return size == 0;
 	}
 
+	/**
+	 * Adds an object to Singly Linked List as a last element ot at tail.
+	 * 
+	 * @param Any Type
+	 * 
+	 */
 	public void add(T t) {
 		addLast(t);
 	}
@@ -61,12 +96,24 @@ public class SinglyLinkedList<T> implements Iterable<T> {
 		size++;
 	}
 
+	/**
+	 * 
+	 * Returns First element in Singly Linked List.
+	 * 
+	 * @return T
+	 */
 	public T peekFirst() {
 		if (isEmpty())
 			throw new RuntimeException("Empty SinglyLinkedList");
 		return head.data;
 	}
 
+	/**
+	 * 
+	 * Removes head or first element and returns that element.
+	 * 
+	 * @return first Element
+	 */
 	public T removeFirst() {
 		if (isEmpty())
 			throw new RuntimeException("SinglyLinkedList is Empty, Can't remove First Element");
@@ -76,6 +123,12 @@ public class SinglyLinkedList<T> implements Iterable<T> {
 		return removed.data;
 	}
 
+	/**
+	 * 
+	 * Removes Last element and returns that element.
+	 * 
+	 * @return Last Element
+	 */
 	public T removeLast() {
 		T data;
 		if (isEmpty())
@@ -95,6 +148,14 @@ public class SinglyLinkedList<T> implements Iterable<T> {
 		return data;
 	}
 
+	/**
+	 * 
+	 * Removes element at provided index if present and returns that element.
+	 * 
+	 * @param index
+	 * @throws IllegalArgumentException
+	 * @return Element
+	 */
 	public T removeAt(int index) {
 		if (index < 0 || index >= size)
 			throw new IllegalArgumentException("Index out of bound");
@@ -118,9 +179,69 @@ public class SinglyLinkedList<T> implements Iterable<T> {
 		return data;
 	}
 
-	// TODO indexOf
-	// TODO contains
-	// TODO remove(Obj)
+	/**
+	 * Returns index of provided element, if present otherwise -1.
+	 * 
+	 * @param obj
+	 * @return
+	 */
+	public int indexOf(Object obj) {
+		int index = 0;
+		Node<T> traverse = head;
+		if (obj == null) {
+			for (; traverse != null; traverse = traverse.next, index++) {
+				if (traverse.data == null)
+					return index;
+			}
+		} else {
+			for (; traverse != null; traverse = traverse.next, index++) {
+				if (obj.equals(traverse.data))
+					return index;
+			}
+		}
+		return -1;
+	}
+
+	/**
+	 * Returns boolean, representing whether provided object is present.
+	 * 
+	 * @param obj
+	 * @return
+	 */
+	public boolean contains(Object obj) {
+		return indexOf(obj) != -1;
+	}
+
+	/**
+	 * Remove provided object, if present and returns that element.
+	 * 
+	 * @param obj
+	 * @return
+	 */
+	public boolean remove(Object obj) {
+		int index = 0;
+		Node<T> traverse = head;
+		if (obj == null) {
+			for (; traverse != null; traverse = traverse.next, index++) {
+				if (traverse.data == null) {
+					removeAt(index);
+					return true;
+				}
+			}
+		} else {
+			for (; traverse != null; traverse = traverse.next, index++) {
+				if (obj.equals(traverse.data)) {
+					removeAt(index);
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+
+	/**
+	 * Returns iterators.
+	 */
 	@Override
 	public Iterator<T> iterator() {
 		return new Iterator<T>() {
@@ -139,6 +260,19 @@ public class SinglyLinkedList<T> implements Iterable<T> {
 			}
 
 		};
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder stringBuilder = new StringBuilder();
+		stringBuilder.append("SinglyLinkedList [size=" + size);
+		Node<T> traverse = head;
+		while (traverse != null) {
+			stringBuilder.append(", "+traverse.data);
+			traverse = traverse.next;
+		}
+		stringBuilder.append("]");
+		return stringBuilder.toString();
 	}
 
 }

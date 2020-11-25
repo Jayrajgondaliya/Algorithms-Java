24456d8f542f35d026abe9f22f092ef413fc7657
Jayraj Gondaliya <jayrajgondaliya@gmail.com>
2020-11-25 15:52:25 -0800

    Adding class file for Queue Implementation

---
+++ src/com/jayrajgondaliya/datastructures/Queue/Queue.java
@@ -0,0 +1,119 @@
+package com.jayrajgondaliya.datastructures.Queue;
+
+import java.util.Iterator;
+
+/**
+ * @author jayrajgondaliya
+ *
+ * @param <T>
+ * 
+ *            Class represents implementation of Data Structure Queue
+ */
+public class Queue<T> implements Iterable<T> {
+
+	/**
+	 * Integer representing number of elements in Queue;
+	 */
+	private int size = 0;
+	Node<T> head = null;
+	Node<T> tail = null;
+
+	private class Node<T> {
+		T data;
+		Node<T> next;
+
+		public Node(T data, Node<T> next) {
+			super();
+			this.data = data;
+			this.next = next;
+		}
+	}
+
+	/**
+	 * @return boolean
+	 * 
+	 *         Returns boolean representing whether Queue is empty
+	 */
+	public boolean isEmpty() {
+		return size == 0;
+	}
+
+	/**
+	 * @return T Returns data from First element.
+	 */
+	public T peek() {
+		if (size == 0)
+			throw new IllegalArgumentException("Empty Queue");
+		return head.data;
+	}
+
+	/**
+	 * @param data
+	 * 
+	 *             Adds element into Queue
+	 */
+	public void enque(T data) {
+		Node<T> node = new Node<T>(data, null);
+		if (size == 0) {
+			head = tail = node;
+		} else {
+			tail.next = node;
+			tail = node;
+		}
+		size++;
+	}
+
+	/**
+	 * @return T Remove element from top and returns value
+	 */
+	public T deque() {
+		if (size == 0)
+			throw new IllegalArgumentException("Empty Queue");
+		T data = head.data;
+		head = head.next;
+		size--;
+		return data;
+	}
+
+	/**
+	 * @return integer
+	 * 
+	 *         Returns integer, representing number of elements in Queue.
+	 */
+	public int size() {
+		return size;
+	}
+
+	@Override
+	public Iterator<T> iterator() {
+		return new Iterator<T>() {
+			Node<T> traverse = head;
+
+			@Override
+			public boolean hasNext() {
+				return traverse != null;
+			}
+
+			@Override
+			public T next() {
+				T data = traverse.data;
+				traverse = traverse.next;
+				return data;
+			}
+
+		};
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder stringBuilder = new StringBuilder("Queue [size=" + size);
+		Node<T> traverse = head;
+		while (traverse != null) {
+			stringBuilder.append(", " + traverse.data);
+			traverse = traverse.next;
+		}
+		stringBuilder.append("]");
+		return stringBuilder.toString();
+	}
+
+}
